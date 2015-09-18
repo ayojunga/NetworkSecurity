485d41e07bcbbbbf11ae922985560e60d9215e9e
ayojunga <umarovjunes@yahoo.com>
2015-09-18 19:09:43 -0400

    second commit

---
+++ Assignment1/Part2/part2.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+cp -r ~/NetworkSecurity/Assignment1/Part1/a ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/b ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/c ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/d ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/e ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/f ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/g ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/h ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/i ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/j ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/k ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/l ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/m ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/n ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/o ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/p ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/q ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/r ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/s ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/t ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/u ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/v ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/w ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/x ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/y ~/NetworkSecurity/Assignment1/Part2
+cp -r ~/NetworkSecurity/Assignment1/Part1/z ~/NetworkSecurity/Assignment1/Part2
+touch reversewords.txt
+letter=z
+cat ~/NetworkSecurity/Assignment1/Part2/z/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=y
+cat ~/NetworkSecurity/Assignment1/Part2/y/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=x
+cat ~/NetworkSecurity/Assignment1/Part2/x/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=w
+cat ~/NetworkSecurity/Assignment1/Part2/w/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=v
+cat ~/NetworkSecurity/Assignment1/Part2/v/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=u
+cat ~/NetworkSecurity/Assignment1/Part2/u/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=t
+cat ~/NetworkSecurity/Assignment1/Part2/t/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=s
+cat ~/NetworkSecurity/Assignment1/Part2/s/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=r
+cat ~/NetworkSecurity/Assignment1/Part2/r/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=q
+cat ~/NetworkSecurity/Assignment1/Part2/q/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=p
+cat ~/NetworkSecurity/Assignment1/Part2/p/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=o
+cat ~/NetworkSecurity/Assignment1/Part2/o/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=n
+cat ~/NetworkSecurity/Assignment1/Part2/n/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=m
+cat ~/NetworkSecurity/Assignment1/Part2/m/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=l
+cat ~/NetworkSecurity/Assignment1/Part2/l/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=k
+cat ~/NetworkSecurity/Assignment1/Part2/k/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=j
+cat ~/NetworkSecurity/Assignment1/Part2/j/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=i
+cat ~/NetworkSecurity/Assignment1/Part2/i/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=h
+cat ~/NetworkSecurity/Assignment1/Part2/h/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=g
+cat ~/NetworkSecurity/Assignment1/Part2/g/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=f
+cat ~/NetworkSecurity/Assignment1/Part2/f/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=e
+cat ~/NetworkSecurity/Assignment1/Part2/e/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=d
+cat ~/NetworkSecurity/Assignment1/Part2/d/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=c
+cat ~/NetworkSecurity/Assignment1/Part2/c/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=b
+cat ~/NetworkSecurity/Assignment1/Part2/b/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+letter=a
+cat ~/NetworkSecurity/Assignment1/Part2/a/assortedwords.txt | grep -i "^$letter" >> ~/NetworkSecurity/Assignment1/Part2/reversewords.txt
+
+rm -r a
+rm -r b
+rm -r c
+rm -r d
+rm -r e
+rm -r f
+rm -r g
+rm -r h
+rm -r i
+rm -r j
+rm -r k
+rm -r l
+rm -r m
+rm -r n
+rm -r o
+rm -r p
+rm -r q
+rm -r r
+rm -r s
+rm -r t
+rm -r u
+rm -r v
+rm -r w
+rm -r x
+rm -r y
+rm -r z
+
+
+
+
+
+
+
